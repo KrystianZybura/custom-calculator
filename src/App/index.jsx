d30284f4fa1e9ea062eb1efb3f3efe575f47830e
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-01 23:58:43 +0100

    initial design added

---
+++ src/App/index.jsx
@@ -0,0 +1,24 @@
+import { Button } from "./Button";
+import { Container } from "./Container";
+
+function App() {
+  return (
+    <Container>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+      <Button>4</Button>
+      <Button>5</Button>
+      <Button>6</Button>
+      <Button>7</Button>
+      <Button>8</Button>
+      <Button>9</Button>
+      <Button>+</Button>
+      <Button>-</Button>
+      <Button>/</Button>
+      <Button>*</Button>
+    </Container>
+  );
+}
+
+export default App;
