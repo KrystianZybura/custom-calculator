690e47044c46d7dba3110bd3a97397c605dcea1b
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-01 23:57:34 +0100

    App.jsx renamed to index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Normalize } from "styled-normalize";
-import App from "./App.jsx";
+import App from "./App/index.jsx";
 import GlobalStyle from "./GlobalStyle.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
