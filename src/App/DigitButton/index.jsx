dd4959c9c2526bf730f2cca40a17af8a4cc18f1e
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-15 18:39:50 +0100

    OnClick interaction added

--- src/App/DigitButton/index.jsx
+++ src/App/DigitButton/index.jsx
@@ -1,5 +1,12 @@
+import { ACTIONS } from "../actions";
 import { Button } from "../styled";
 
-const DigitButton = ({ digit }) => <Button>{digit}</Button>;
+const DigitButton = ({ digit, dispatch }) => (
+  <Button
+    onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+  >
+    {digit}
+  </Button>
+);
 
 export default DigitButton;
